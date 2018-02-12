0cb1562f18945020c5cde62397a671a0c0f92563
Liam Barstad <liambarstad@gmail.com>
2018-02-12 11:06:54 -0700

    remove user story from test (oops)

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,9 +1,3 @@
-As a user,
-   when I visit Word Watch
-   I should see the top used word and its count (based on the Word Watch API database)
-  in the "Top Word: " heading.
-  e.g., "Top Word: ciabatta (22)"
-
 import { expect } from "chai"
 import sinon from "sinon"
 
