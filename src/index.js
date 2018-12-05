753f0084e0224d9f4f19e4fc74eabc68e630f07e
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-05 14:24:42 -0700

    Adds post request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import $ from 'jquery'
-// import pry from 'pryjs'
 
 $(document).ready(() => {
 
@@ -11,7 +10,31 @@ fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
 const displayWord = (word) => {
   const topWord = Object.keys(word.word)[0];
   const topWordCount = Object.values(word.word)[0];
-  $('.top-word h3').append(`"${topWord}" = ${topWordCount} times`);
+  $('.top-word h3').append(`"${topWord}"`);
+  $('.word-count').append(topWordCount);
 }
 
+const sendWords = () => {
+  const words = $('.text-submission textarea').val();
+  words.split(" ").forEach(word => {
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      word: { value: word }
+    })
+  })
+   .then(response => response.json())
+   .then(message => displayMessage(message))
+   .catch(error => console.log({ error }));
+});
+};
+
+const displayMessage = (message) => {
+  const parsedMessage = Object.values(message);
+  $('.text-submission textarea').val('');
+  $('.word-count').append(`<p>${parsedMessage}</p>`);
+};
+
+$('.text-submission button').on('click', sendWords);
 })
