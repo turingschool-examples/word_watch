d76d666ce4e931604c414c547b0d4d8351fbc701
ClaireBvs <clairembeauvais@gmail.com>
2018-12-05 13:40:20 -0700

    Add fetch call to Heroku api/v1/top_word and function topWord [#1]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+    .then(response => response.json())
+    .then(json => topWord(json))
+    .catch(error => console.log(error))
 })
+
+function topWord(data) {
+  var word = Object.keys(data.word)[0]
+  var value = data.word[word];
+  $('.top-word').append(`<p>${word} is the most popular word with ${value} entries</p>`)
+}
