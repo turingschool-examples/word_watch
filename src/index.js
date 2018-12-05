8803cfd6c3d0abcf03d44d30d61bc68edf3a75bb
ClaireBvs <clairembeauvais@gmail.com>
2018-12-05 14:01:40 -0700

    Add event listener click and fetch POST /api/v1/words [#2]

--- src/index.js
+++ src/index.js
@@ -5,11 +5,25 @@ $(document).ready(() => {
     .then(response => response.json())
     .then(json => topWord(json))
     .catch(error => console.log(error))
-})
 
-function topWord(data) {
-  var word = Object.keys(data.word)[0]
-  var value = data.word[word];
-  $('.top-word').append(`<p>${word} is the most popular word </p>`)
-  $('.word-count').append(`<p>Count : ${value} </p>`)
-}
+  function topWord(data) {
+    var word = Object.keys(data.word)[0]
+    var value = data.word[word];
+    $('.top-word').append(`<p>${word} is the most popular word </p>`)
+    $('.word-count').append(`<p>Count : ${value} </p>`)
+  }
+
+  $('#submit-word').on('click', function() {
+    var word = $('#word-input').val()
+
+    fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        word: { value: word }
+      })
+    })
+  })
+})
