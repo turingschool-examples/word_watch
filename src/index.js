4f5404c5e430f5c36187d193aa50453bbd1686a7
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-05 13:49:17 -0700

    Add JS methods to fetch Top word from API and display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import $ from 'jquery'
 
-$(document).ready(() => {
-  // have fun!
-})
+fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+.then(response => response.json())
+.then(response => displayTopWord(response))
+.catch(error => console.error({ error }));
+
+  const displayTopWord = (wordResponse) => {
+    const topWord = Object.keys(wordResponse.word)[0];
+    const topWordCount = Object.values(wordResponse.word)[0];
+    $('.top-word').children("h3").after(`<p>${topWord} occurs ${topWordCount} times!</p>`)
+  }
