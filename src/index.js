e06f95698e8b2e4577e75f709d170bacf58914c6
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-05 14:07:39 -0700

    Add fetch POST method for incoming words, breaks string into array of individual words because API accepts one word per POST (see API controller)

--- src/index.js
+++ src/index.js
@@ -5,8 +5,26 @@ fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
 .then(response => displayTopWord(response))
 .catch(error => console.error({ error }));
 
-  const displayTopWord = (wordResponse) => {
-    const topWord = Object.keys(wordResponse.word)[0];
-    const topWordCount = Object.values(wordResponse.word)[0];
-    $('.top-word').children("h3").after(`<p>${topWord} occurs ${topWordCount} times!</p>`)
-  }
+const displayTopWord = (wordResponse) => {
+  const topWord = Object.keys(wordResponse.word)[0];
+  const topWordCount = Object.values(wordResponse.word)[0];
+  $('.top-word').children("h3").after(`<p>${topWord} occurs ${topWordCount} times!</p>`)
+}
+
+$("#break-down").on("click", () => {
+  var text = $("#breakdown-text").val();
+  var wordArray = text.split(" ");
+  wordArray.forEach(function(word) {
+    let payload = {
+      "word": {
+        "value": word
+      }
+    };
+    fetch("https://wordwatch-api.herokuapp.com/api/v1/words", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    })
+    .then(response => console.log(response.json()));
+  });
+});
