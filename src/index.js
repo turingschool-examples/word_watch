3ca18848f57eea5b8ca29d1eaf13409aff8d6e39
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-05 11:14:23 -0700

    Add functionality to take in words

--- src/index.js
+++ src/index.js
@@ -6,11 +6,42 @@ function getTopWord(){
     url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
     success: function(word){
       var top_word = Object.keys(word.word)[0]
-      $(".word-count").html(`<h3>${top_word}</h3> <br> <h3>${word.word[top_word]}</h3>`)}
-
+      $(".word-count").html(`<h3>${top_word}<br>count:${word.word[top_word]}</h3>`)}
   })
 }
 
+function splitWords(){
+  var text_to_post = document.getElementById("text-to-post").value;
+  var array_of_words = text_to_post.split(" ")
+
+  array_of_words.forEach(word =>{
+    postWords(word)
+  })
+}
+
+function postWords(word_to_post){
+  var word_to_post = word_to_post
+  var url = "https://wordwatch-api.herokuapp.com/api/v1/words";
+  var payload = {
+      "word": {
+        "value": `${word_to_post}`,
+      }
+    };
+
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Accept': 'application/json',
+      'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    })
+    .catch(error => console.error(error))
+}
+
+
 $(document).ready(() => {
   getTopWord()
+
+  $( "#break-down" ).on("click", function(){
+    splitWords();
+  })
 })
