a9c2d13a17c94c0da161dad4429d8429c5f9812d
Harper Bellows <harper.bellows@gmail.com>
2019-02-05 09:45:48 -0700

    Add dream driven getTopWord and displayTopWord functions to js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import $ from 'jquery'
 
+const productionUrl = 'https://wordwatch-api.herokuapp.com';
+const getWord = '/api/v1/top_word';
+const postWord = '/api/v1/words';
+
 $(document).ready(() => {
-  // have fun!
+  getTopWord()
 })
+
+const getTopWord = () => {
+  let url = `${productionUrl}` + `${getWord}`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    displayTopWord(data)
+  })
+  .catch(error => {
+    console.log(error)
+  });
+};
+
+
+const displayTopWord = () => {
+
+}
