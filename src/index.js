dbda1afae59c2daa9b63459805ee7e00cec89886
normanrs <normanrs@live.com>
2019-02-05 10:21:43 -0700

    Getting top word with count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import $ from 'jquery'
 
+function getTopword() {
+fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word`)
+  .then(response => response.json())
+  .then(word => appendTopword(word))
+  .catch(error => console.error({ error }));
+}
+
+function appendTopword (word) {
+  let theWord = Object.keys(word.word)[0];
+  let theCount = Object.values(word.word)[0];
+  $('#topword').append(`
+      Top Word: "<span class="name">${theWord}"</span></br>
+      Count: <span class="name">${theCount}</span>
+  `);
+};
+
 $(document).ready(() => {
   // have fun!
+  getTopword();
+
 })
