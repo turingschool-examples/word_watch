d3d6debe4e1543af60f478ddd2959ebfd5c0f52e
Cesar Jolibois <diazcd85@gmail.com>
2019-02-05 10:21:28 -0700

    create functinality to display the most listed word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+
 import $ from 'jquery'
 
+function  presentWord() {
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word', {
+    method: "GET",
+    redirect: 'follow',
+    headers: {
+      'Accept': 'application/json',  'Content-Type': 'application/json' },
+  })
+  .then(res => res.json())
+  .then(data => {
+    var wordKey = Object.keys(data.word)[0]
+    var text = `<h3 class="top">${wordKey}, Times listed: ${data.word[wordKey]}</h3>`
+    $('.top-word').append(text);
+  })
+}
+
 $(document).ready(() => {
-  // have fun!
+  presentWord();
 })
