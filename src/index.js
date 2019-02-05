e76fbef9a5c973debcf8695bab58a551219afd62
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-05 10:38:43 -0700

    Add fuctionality to display top word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import $ from 'jquery'
 
+function getTopWord(){
+  $.ajax({
+    type: "GET",
+    url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
+    success: function(word){
+      var top_word = Object.keys(word.word)[0]
+      $(".word-count").html(`<h3>${top_word}</h3> <br> <h3>${word.word[top_word]}</h3>`)}
+
+  })
+}
+
 $(document).ready(() => {
-  // have fun!
+  getTopWord()
 })
