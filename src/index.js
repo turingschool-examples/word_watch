e0fc9e2533329e6d29a65eed5a7d311c285ffb4a
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-05 09:26:17 -0700

    GET request to API is returing data I want

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import $ from 'jquery'
 
+var apiURL = "https://wordwatch-api.herokuapp.com/api/v1"
+var wordRequest = new XMLHttpRequest();
+
 $(document).ready(() => {
-  // have fun!
+  getTopWord();
 })
+
+function getTopWord() {
+  wordRequest.open('GET', apiURL + "/top_word")
+    wordRequest.onload = function() {
+      var word_data = JSON.parse(wordRequest.responseText)
+
+      appendTopWord(word_data)
+    };
+    wordRequest.send();
+}
+
+function appendTopWord(word_data) {
+  var top_word = Object.keys(word_data.word);
+  var word_count = word_data.word.top_word
+  debugger;
+  
+}
