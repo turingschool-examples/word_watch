bf19a3df4aa90786cc4836230f056d0469fe6357
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-05 09:57:12 -0700

    get and display top word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  getTopUser();
 })
+
+const getTopUser = () => {
+  let url = 'https://wordwatch-api.herokuapp.com/api/v1/top_word';
+  fetch(url)
+  .then( response => response.json() )
+  .then( data => {
+    let word = Object.keys(data["word"])[0];
+    let count = data["word"][word];
+    $(".top-word h3").text(`Top word from Word Watch API: ${word}`);
+    $("#word-count").text(`Count: ${count}`)
+  })
+}
