58c714c7536b5b0ac89292e67743465cceaf91be
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-05 12:02:54 -0600

    updates bundle.js

--- docs/bundle.js
+++ docs/bundle.js
@@ -82,7 +82,37 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 __WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).ready(() => {
-  // have fun!
+
+    fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+    .then(response => response.json())
+    .then(top_word => displayWord(top_word))
+    .catch(error => console.error({ error }))
+
+    const displayWord = (input) => {
+      var word = Object.keys(input.word)[0];
+      var wordCount = Object.values(input.word)[0];
+      return __WEBPACK_IMPORTED_MODULE_0_jquery___default()('.top-word h3').append(`"${word}", ${wordCount} times`);
+    }
+
+  __WEBPACK_IMPORTED_MODULE_0_jquery___default()( "#breakdown-button" ).click(function() {
+      var input = __WEBPACK_IMPORTED_MODULE_0_jquery___default()('#text-input').val();
+        fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+          method: "POST",
+          headers: {
+             "Content-Type": "application/json; charset=utf-8",
+          },
+          body: JSON.stringify({ word: { value: input}})
+        })
+        .then(response => response.json())
+        .then(json => {
+          fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+          .then(response => response.json())
+          .then(top_word => displayWord(top_word))
+          .catch(error => console.log({ error }));
+        })
+        .catch(error => console.log({ error }));
+    });
+
 })
 
 
