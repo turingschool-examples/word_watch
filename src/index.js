383bf4586d7ec539235a43106c158094cec8721f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-05 11:03:01 -0600

    creates fetch request taht gets the top word from the WordWatch API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+
+    fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+    .then(response => response.json())
+    .then(top_word => displayWord(top_word))
+    .catch((error) => console.error({ error }))
+
 })
