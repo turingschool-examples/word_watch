ba3a9087b145313a6f9b3b62c10dd8a890cfe403
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-05 12:02:35 -0600

    adds post request

--- src/index.js
+++ src/index.js
@@ -5,12 +5,31 @@ $(document).ready(() => {
     fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
     .then(response => response.json())
     .then(top_word => displayWord(top_word))
-    .catch((error) => console.error({ error }))
+    .catch(error => console.error({ error }))
 
     const displayWord = (input) => {
       var word = Object.keys(input.word)[0];
       var wordCount = Object.values(input.word)[0];
       return $('.top-word h3').append(`"${word}", ${wordCount} times`);
     }
-    
+
+  $( "#breakdown-button" ).click(function() {
+      var input = $('#text-input').val();
+        fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+          method: "POST",
+          headers: {
+             "Content-Type": "application/json; charset=utf-8",
+          },
+          body: JSON.stringify({ word: { value: input}})
+        })
+        .then(response => response.json())
+        .then(json => {
+          fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+          .then(response => response.json())
+          .then(top_word => displayWord(top_word))
+          .catch(error => console.log({ error }));
+        })
+        .catch(error => console.log({ error }));
+    });
+
 })
