c2e7df7785e819ffe3388cc1fc8033932e92fa7c
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-22 11:28:07 -0600

    Add ajax request to get the top word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  var url = 'https://wordwatch-api.herokuapp.com'
+  $.ajax({
+    url: url + '/api/v1/top_word',
+    method: "GET",
+    dataType: 'json',
+    success: function(response){
+      let word = Object.keys(response.word)[0];
+      let count = Object.values(response.word)[0];
+      $('.word-count').html(`<h3>Word: ${word} Count: ${count}</h3>`)
+    }
+  })
 })
