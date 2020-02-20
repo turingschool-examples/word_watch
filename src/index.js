18a9beb6cf711defc167315dc781ddc4e3e4a0b5
Laura Schulz <lrs8810@gmail.com>
2020-02-20 10:50:06 -0700

    Add functionality for showing the top word and count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word`)
+  .then(response => response.json())
+  .then(result => {
+    var topWord = result.word
+    var final = JSON.stringify(topWord)
+    return document.querySelector('.word-count').innerHTML = final;
+  });
 })
