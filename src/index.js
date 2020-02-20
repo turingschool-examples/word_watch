2b431855a9fd47f9db3f9085c4466c88db17dfca
Laura Schulz <lrs8810@gmail.com>
2020-02-20 12:03:48 -0700

    Add functionality for posting to API with input text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+var fetch = require('node-fetch')
 
 $(document).ready(() => {
   fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word`)
@@ -8,4 +9,25 @@ $(document).ready(() => {
     var final = JSON.stringify(topWord)
     return document.querySelector('.word-count').innerHTML = final;
   });
+})
+
+$(document).ready(() => {
+  var targetElement = document.querySelector('.submit')
+  targetElement.addEventListener("click", function() {
+    var input = document.getElementsByClassName('input')[0].value
+    var words = input.split(" ")
+
+    words.forEach(element => {
+      console.log(element)
+      var wordToSend = { "word": { "values": `${element}` }}
+      fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(wordToSend)
+      })
+      .then(function(response) {
+        console.log(response)
+      })
+    })
+  })
 })
